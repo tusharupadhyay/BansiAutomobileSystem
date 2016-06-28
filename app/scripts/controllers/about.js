66807b4f8325b5b8028898abeb3013d0eea2da0f
Sahil Patayar <sahilpatayar1989@gmail.com>
2016-06-27 21:20:57 -0500

    Initial comment - Project set up

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bansiAutomobileSystemApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bansiAutomobileSystemApp
+ */
+angular.module('bansiAutomobileSystemApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bansiAutomobileSystemApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bansiAutomobileSystemApp
+ */
+angular.module('bansiAutomobileSystemApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
